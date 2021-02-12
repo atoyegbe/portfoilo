5491cd9f50d385ab0c4eaff57e3884b34603eedb
Adeyemi Atoyegbe <herdeyemie@gmail.com>
2021-02-12 01:30:38 +0100

    changes

--- portfolio/portfolio/wsgi.py
+++ portfolio/portfolio/wsgi.py
@@ -3,7 +3,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 
 application = get_wsgi_application()
