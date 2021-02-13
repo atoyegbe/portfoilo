4893780384da6dd1f17e64e11f36437dc90faadd
Adeyemi Atoyegbe <herdeyemie@gmail.com>
2021-02-13 23:07:55 +0100

    fix

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'www.deyemie.me']
+ALLOWED_HOSTS = ['www.deyemie.me', 'deyemie.herokuapp.com'] 
 
 
 # Application definition
